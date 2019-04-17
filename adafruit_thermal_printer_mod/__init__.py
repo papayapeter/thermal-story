003857e8c0d3e639f5e1a1130e001d6fbe0ac704
PapayaPeter <t.babel@gmx.com>
2019-04-17 15:13:34 +0200

    20

--- adafruit_thermal_printer_mod/__init__.py
+++ adafruit_thermal_printer_mod/__init__.py
@@ -12,9 +12,9 @@ def get_printer_class(version):
     assert version is not None
     assert version >= 0.0
     if version < 2.64:
-        import adafruit_thermal_printer.thermal_printer_legacy_mod as thermal_printer
+        import adafruit_thermal_printer_mod.thermal_printer_legacy_mod as thermal_printer
     elif version < 2.68:
-        import adafruit_thermal_printer.thermal_printer_264_mod as thermal_printer
+        import adafruit_thermal_printer_mod.thermal_printer_264_mod as thermal_printer
     else:
-        import adafruit_thermal_printer.thermal_printer_mod as thermal_printer
+        import adafruit_thermal_printer_mod.thermal_printer_mod as thermal_printer
     return thermal_printer.ThermalPrinter
