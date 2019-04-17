6a1c27ac193f7718855c487712e85c718aad93f2
PapayaPeter <t.babel@gmx.com>
2019-04-17 14:41:49 +0200

    16

---
+++ thermal-story.py
@@ -0,0 +1,82 @@
+# zeno gries
+# thermal story
+# 2019
+# adafruit library version (no ä, ö, ü, ß)
+
+import os
+import serial
+import adafruit_thermal_printer_mod
+import re
+
+# establish objects
+uart = serial.Serial('/dev/serial0', baudrate=9600, timeout=3000)
+ThermalPrinter = adafruit_thermal_printer.get_printer_class(2.64)
+
+printer = ThermalPrinter(uart)
+
+# open file once (to make sure it is there)
+log = open('log.txt', 'a+')
+log.close()
+
+# main loop
+while (True):
+    # clear screen
+    os.system('clear')
+
+    # check for paper
+    if printer.has_paper():
+        # print intruction
+        print('Schreib die Geschichte weiter.\n\n')
+        # print story so far line by line
+        for line in open('log.txt'):
+            print(line)
+        # wait for input
+        text = input("> ")
+
+        # sort out special commands
+        if text == '--test': # print test page
+            printer.test_page()
+            printer.feed(2)
+        elif text == '--quit': # quit program
+            break
+        elif text == '--feed': # feed to lines of paper
+            printer.feed(2)
+        elif text == '--clear': # clear all logs
+                os.remove('log.txt')
+                log = open('log.txt', 'w+')
+                log.close()
+        else: # print
+            # substitute german characters
+            tmp = ''
+            for c in text:
+                if c == 'Ä':
+                    tmp += 'Ae'
+                elif c == 'ä':
+                    tmp += 'ae'
+                elif c == 'Ö':
+                    tmp += 'Oe'
+                elif c == 'ö':
+                    tmp += 'oe'
+                elif c == 'Ü':
+                    tmp += 'Ue'
+                elif c == 'ü':
+                    tmp += 'ue'
+                elif c == 'ß':
+                    tmp += 'ss'
+                else:
+                    tmp += c
+
+            # sort out unsupported characters
+            to_print = ''
+            to_print = re.sub(r'[^\x00-\x7F]', '#', tmp)
+
+            # write to log and print
+            log = open('log.txt', 'a+')
+            log.write(to_print + '\n')
+            log.close()
+
+            printer.print(to_print)
+    else: # if there is no paper then print that or quit
+        text = input('Papier muss nachgelegt werden. Frag an der Bar nach.')
+        if text == '--quit':
+            break
